79b17bdc702727cc9b3b6f33a70280f1502c8daa
volodlo <143138221+volodlo@users.noreply.github.com>
2023-08-30 11:01:50 +0700

    Update 3-2-2.py
    Исправил задание

--- Module 3/3-2-2.py
+++ Module 3/3-2-2.py
@@ -1,10 +1,9 @@
 from random import randint
-from functools import reduce
 n = int(input())
 m = [[randint(0,100) for i in range(n)] for j in range(n)]
+large = []
 print(m)
-c = sorted(m)
-result = c[-1]
-print(result)
-x = reduce(max, result)
-print(x)
+for k in m:
+    for i in k:
+        large.append(i)
+print(max(large))
